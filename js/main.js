bb20469631f334fb81b6d855767a6b55de453950
Nicholas Leoutsakos <nleoutsa@gmail.com>
2015-09-08 22:47:34 -0400

    performance ops, lose resize on hover feature for now..

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@
 
 // DATA
 var artwork = [
-    {title: 'a', small_thumbnail: 'http://i.imgur.com/KuEXstZt.jpg', medium_thumbnail: 'http://i.imgur.com/KuEXstZl.jpg', large_pic: 'http://i.imgur.com/KuEXstZ.jpg'},
-    {title: 'b', small_thumbnail: 'http://i.imgur.com/kfpjNWXt.png', medium_thumbnail: 'http://i.imgur.com/kfpjNWXl.png', large_pic: 'http://i.imgur.com/kfpjNWX.png'},
-    {title: 'c', small_thumbnail: 'http://i.imgur.com/fm3ww0et.jpg', medium_thumbnail: 'http://i.imgur.com/fm3ww0el.jpg', large_pic: 'http://i.imgur.com/fm3ww0e.jpg'},
-    {title: 'aa', small_thumbnail: 'http://i.imgur.com/J0pmOlVt.jpg', medium_thumbnail: 'http://i.imgur.com/J0pmOlVl.jpg', large_pic: 'http://i.imgur.com/J0pmOlV.jpg'},
-    {title: 'bb', small_thumbnail: 'http://i.imgur.com/qBKO2GSt.jpg', medium_thumbnail: 'http://i.imgur.com/qBKO2GSl.jpg', large_pic: 'http://i.imgur.com/qBKO2GS.jpg'},
-    {title: 'cc', small_thumbnail: 'http://i.imgur.com/WdR9k3Zt.jpg', medium_thumbnail: 'http://i.imgur.com/WdR9k3Zl.jpg', large_pic: 'http://i.imgur.com/WdR9k3Z.jpg'},
-    {title: 'a', small_thumbnail: 'http://i.imgur.com/KuEXstZt.jpg', medium_thumbnail: 'http://i.imgur.com/KuEXstZl.jpg', large_pic: 'http://i.imgur.com/KuEXstZ.jpg'},
-    {title: 'b', small_thumbnail: 'http://i.imgur.com/kfpjNWXt.png', medium_thumbnail: 'http://i.imgur.com/kfpjNWXl.png', large_pic: 'http://i.imgur.com/kfpjNWX.png'},
-    {title: 'c', small_thumbnail: 'http://i.imgur.com/fm3ww0et.jpg', medium_thumbnail: 'http://i.imgur.com/fm3ww0el.jpg', large_pic: 'http://i.imgur.com/fm3ww0e.jpg'},
-    {title: 'aa', small_thumbnail: 'http://i.imgur.com/J0pmOlVt.jpg', medium_thumbnail: 'http://i.imgur.com/J0pmOlVl.jpg', large_pic: 'http://i.imgur.com/J0pmOlV.jpg'},
-    {title: 'bb', small_thumbnail: 'http://i.imgur.com/qBKO2GSt.jpg', medium_thumbnail: 'http://i.imgur.com/qBKO2GSl.jpg', large_pic: 'http://i.imgur.com/qBKO2GS.jpg'},
-    {title: 'cc', small_thumbnail: 'http://i.imgur.com/WdR9k3Zt.jpg', medium_thumbnail: 'http://i.imgur.com/WdR9k3Zl.jpg', large_pic: 'http://i.imgur.com/WdR9k3Z.jpg'}
+    {title: 'a', small_thumbnail: 'http://i.imgur.com/KuEXstZt.jpg', medium_thumbnail: 'http://i.imgur.com/KuEXstZm.jpg', large_pic: 'http://i.imgur.com/KuEXstZ.jpg'},
+    {title: 'b', small_thumbnail: 'http://i.imgur.com/kfpjNWXt.png', medium_thumbnail: 'http://i.imgur.com/kfpjNWXm.png', large_pic: 'http://i.imgur.com/kfpjNWX.png'},
+    {title: 'c', small_thumbnail: 'http://i.imgur.com/fm3ww0et.jpg', medium_thumbnail: 'http://i.imgur.com/fm3ww0em.jpg', large_pic: 'http://i.imgur.com/fm3ww0e.jpg'},
+    {title: 'aa', small_thumbnail: 'http://i.imgur.com/J0pmOlVt.jpg', medium_thumbnail: 'http://i.imgur.com/J0pmOlVm.jpg', large_pic: 'http://i.imgur.com/J0pmOlV.jpg'},
+    {title: 'bb', small_thumbnail: 'http://i.imgur.com/qBKO2GSt.jpg', medium_thumbnail: 'http://i.imgur.com/qBKO2GSm.jpg', large_pic: 'http://i.imgur.com/qBKO2GS.jpg'},
+    {title: 'cc', small_thumbnail: 'http://i.imgur.com/WdR9k3Zt.jpg', medium_thumbnail: 'http://i.imgur.com/WdR9k3Zm.jpg', large_pic: 'http://i.imgur.com/WdR9k3Z.jpg'},
+    {title: 'a', small_thumbnail: 'http://i.imgur.com/KuEXstZt.jpg', medium_thumbnail: 'http://i.imgur.com/KuEXstZm.jpg', large_pic: 'http://i.imgur.com/KuEXstZ.jpg'},
+    {title: 'b', small_thumbnail: 'http://i.imgur.com/kfpjNWXt.png', medium_thumbnail: 'http://i.imgur.com/kfpjNWXm.png', large_pic: 'http://i.imgur.com/kfpjNWX.png'},
+    {title: 'c', small_thumbnail: 'http://i.imgur.com/fm3ww0et.jpg', medium_thumbnail: 'http://i.imgur.com/fm3ww0em.jpg', large_pic: 'http://i.imgur.com/fm3ww0e.jpg'},
+    {title: 'aa', small_thumbnail: 'http://i.imgur.com/J0pmOlVt.jpg', medium_thumbnail: 'http://i.imgur.com/J0pmOlVm.jpg', large_pic: 'http://i.imgur.com/J0pmOlV.jpg'},
+    {title: 'bb', small_thumbnail: 'http://i.imgur.com/qBKO2GSt.jpg', medium_thumbnail: 'http://i.imgur.com/qBKO2GSm.jpg', large_pic: 'http://i.imgur.com/qBKO2GS.jpg'},
+    {title: 'cc', small_thumbnail: 'http://i.imgur.com/WdR9k3Zt.jpg', medium_thumbnail: 'http://i.imgur.com/WdR9k3Zm.jpg', large_pic: 'http://i.imgur.com/WdR9k3Z.jpg'}
 ];
 
 
@@ -40,8 +40,8 @@ window.onload = function(event) {
     changeFrameSizes(num_rows)
 
     artwork.forEach(function(piece) {
-    createFrame(piece);
-});
+        createFrame(piece);
+    });
 }
 
 // resize
@@ -84,13 +84,13 @@ function setRowSize (width) {
 function changeFrameSizes (num_rows) {
 
     if (num_rows == 4) {
-        zoomed_size = '47%';
+        zoomed_size = '95%';
         enlarged_size = '25%';
         normal_size = '22%';
         smaller_size = '21%';
     }
     else if (num_rows == 3) {
-        zoomed_size = '60%';
+        zoomed_size = '95%';
         enlarged_size = '33%';
         normal_size = '30%';
         smaller_size = '29%';
@@ -115,13 +115,13 @@ function changeFrameSizes (num_rows) {
 }
 
 function createFrame (piece) {
-    var piece = piece;
     var frame = elt('div', 'frame');
     var art_piece = elt('div', 'art_piece');
     var aspect_force = elt('div', 'aspect_force');
 
     frame.style.width = normal_size;
 
+    // set data attributes so we can access them elsewhere...
     art_piece.dataset.large_pic = piece.large_pic;
     art_piece.dataset.medium_thumbnail = piece.medium_thumbnail;
     art_piece.dataset.image_url = piece.small_thumbnail;
@@ -136,14 +136,9 @@ function createFrame (piece) {
     gallery.appendChild(frame);
 }
 
-function zoom(node, hidden_array) {
-    hidden_array.forEach(function(element) {
-        element.style.width = 0;
-        element.style.margin = 0;
-    });
+function zoom(node) {
 
     var ratio = getImageRatio(node.childNodes[1].dataset.image_url);
-
 
     node.childNodes[1].style.background = 'url(' + node.childNodes[1].dataset.large_pic; + ')';
     node.childNodes[1].style.backgroundSize = 'cover';
@@ -151,14 +146,8 @@ function zoom(node, hidden_array) {
     node.style.width = zoomed_size;
 }
 
-function unzoom(node, hidden_array) {
+function unzoom(node) {
     node.childNodes[0].style.paddingTop = '100%';
-
-    hidden_array.forEach(function(element) {
-        element.style.width = normal_size;
-        element.style.marginLeft = "2.5%";
-        element.style.marginTop = "2.5%";
-    });
 
     node.style.width = normal_size;
 }
@@ -170,54 +159,54 @@ function unzoom(node, hidden_array) {
 //         EVENT LISTENERS
 //////////////////////////////////////////
 
-addEventListener('mouseover', function(event) {
-    if (event.target.className == 'art_piece') {
+// addEventListener('mouseover', function(event) {
+//     if (event.target.className == 'art_piece') {
 
-        event.target.style.opacity = 1;
+//         event.target.style.opacity = 1;
 
-        if (event.target.dataset.zoom == 'false') {
-            event.target.parentNode.style.width = enlarged_size;
-        }
-
-
-
-        // get index of picture in gallery
-        var index = getIndex(event.target.parentNode)
-
-        var this_row = Math.floor(index / row_width);
+//         if (event.target.dataset.zoom == 'false') {
+//             event.target.parentNode.style.width = enlarged_size;
+//         }
 
 
-        for (var i = 0; i < gallery.childNodes.length; i++) {
-            var child = gallery.childNodes[i];
-            if (child != event.target.parentNode && (this_row == Math.floor(i / row_width))) {
-                child.style.width = smaller_size;
-                child.style.marginBottom = "2%";
 
-            }
-        }
+//         // get index of picture in gallery
+//         var index = getIndex(event.target.parentNode)
 
-    }
-});
-addEventListener('mouseout', function(event) {
-    if (event.target.className == 'art_piece') {
+//         var this_row = Math.floor(index / row_width);
 
-        // get index of picture in gallery
-        var index = getIndex(event.target.parentNode)
 
-        event.target.parentNode.childNodes[0].style.paddingTop = '100%';
+//         for (var i = 0; i < gallery.childNodes.length; i++) {
+//             var child = gallery.childNodes[i];
+//             if (child != event.target.parentNode && (this_row == Math.floor(i / row_width))) {
+//                 child.style.width = smaller_size;
+//                 child.style.marginBottom = "2%";
 
-        event.target.dataset.zoom = 'false';
+//             }
+//         }
 
-        for (var i = 0; i < gallery.childNodes.length; i++) {
-            var child = gallery.childNodes[i];
+//     }
+// });
+// addEventListener('mouseout', function(event) {
+//     if (event.target.className == 'art_piece') {
 
-            child.style.width = normal_size;
-            child.style.marginBottom = "0";
-            child.style.marginLeft = "2.5%";
-            child.style.marginTop = "2.5%";
-        }
-    }
-});
+//         // get index of picture in gallery
+//         var index = getIndex(event.target.parentNode)
+
+//         event.target.parentNode.childNodes[0].style.paddingTop = '100%';
+
+//         event.target.dataset.zoom = 'false';
+
+//         for (var i = 0; i < gallery.childNodes.length; i++) {
+//             var child = gallery.childNodes[i];
+
+//             child.style.width = normal_size;
+//             child.style.marginBottom = "0";
+//             child.style.marginLeft = "2.5%";
+//             child.style.marginTop = "2.5%";
+//         }
+//     }
+// });
 
 
 addEventListener('click', function(event) {
@@ -226,22 +215,17 @@ addEventListener('click', function(event) {
         // get index of picture in gallery
         var index = getIndex(event.target.parentNode)
 
-        var hidden_elements = [];
-        for (var i = 0; i < (index % row_width); i++) {
-            hidden_elements.push(gallery.childNodes[index - (i + 1)]);
-        }
-
         // zoom
         if (event.target.dataset.zoom == 'false') {
 
-            zoom(event.target.parentNode, hidden_elements);
+            zoom(event.target.parentNode);
 
             event.target.dataset.zoom = 'true';
         }
         // reset positioning
         else if (event.target.dataset.zoom == 'true') {
 
-            unzoom(event.target.parentNode, hidden_elements);
+            unzoom(event.target.parentNode);
 
             event.target.dataset.zoom = 'false';
         }
