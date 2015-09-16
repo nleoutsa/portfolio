5303fdcc598a953aabd0f57dd25452521d313c21
Nicholas Leoutsakos <nleoutsa@gmail.com>
2015-09-16 15:39:18 -0400

    menu for mobile

--- js/main.js
+++ js/main.js
@@ -18,6 +18,9 @@ var toggle_switches = document.getElementsByClassName('toggle');
 var tag_buttons = document.getElementsByClassName('tag');
 var about_buttons = document.getElementsByClassName('about');
 
+var menu_toggle = document.getElementById('menu_toggle');
+var menu = document.getElementById('menu');
+
 var git_repos = [];
 //////////////////////////////////////////
 //         FUNCTION CALLS
@@ -33,6 +36,81 @@ project_data.forEach(function(piece) {
 // make about section a frame element fitting into gallery...
 // you can use id to over specify frame styles...
 
+
+
+//////////////////////////////////////////
+//         EVENT LISTENERS
+//////////////////////////////////////////
+
+window.onresize=function(){
+    if (getWindowWidth().x > 900) {
+        menu.style.left = '0%';
+        gallery.parentNode.style.left = '0%';
+    }
+    else {
+        menu.style.left = '100%';
+        gallery.parentNode.style.left = '0%';
+    }
+};
+
+// add event listeners to art_pieces
+for (var i = 0; i < art_pieces.length; i++) {
+    art_pieces[i].addEventListener('click', function(event) {click_artpiece(event);}, false);
+    if (getWindowWidth().x > 600) {
+        art_pieces[i].addEventListener('mouseover', function(event) {mouseover_artpiece(event);}, false);
+        art_pieces[i].addEventListener('mouseout', function(event) {mouseout_artpiece(event);}, false);
+    }
+}
+
+// add event listeners to toggle_switches
+for (var i = 0; i < toggle_switches.length; i++) {
+    toggle_switches[i].addEventListener('click', function(event) {toggleCategory(event);}, false);
+}
+
+// add event listeners to tag buttons
+for (var i = 0; i < tag_buttons.length; i++) {
+    tag_buttons[i].addEventListener('click', function(event) {
+        showCategory(event);
+
+        if (getWindowWidth().x < 900) {
+            menu.style.left = '100%';
+            gallery.parentNode.style.left = '0%';
+        }
+
+    }, false);
+}
+
+// add event listeners to about buttons
+for (var i = 0; i < about_buttons.length; i++) {
+    about_buttons[i].addEventListener('click', function(event) {
+        showAboutSection(event);
+
+        if (getWindowWidth().x < 900) {
+            menu.style.left = '100%';
+            gallery.parentNode.style.left = '0%';
+        }
+
+    }, false);
+}
+
+menu_toggle.addEventListener('click', function() {
+    if (menu.style.left != '0%') {
+        menu.style.left = '50%';
+        gallery.parentNode.style.left = '-100%';
+    }
+    else {
+        menu.style.left = '100%';
+        gallery.parentNode.style.left = '0%';
+    }
+});
+
+gallery.parentNode.addEventListener('click', function() {
+
+    if (getWindowWidth().x < 900) {
+        menu.style.left = '100%';
+        gallery.parentNode.style.left = '0%';
+    }
+});
 
 
 
@@ -238,35 +316,6 @@ function unzoom(node) {
     node.dataset.zoom = 'false';
 }
 
-
-
-//////////////////////////////////////////
-//         EVENT LISTENERS
-//////////////////////////////////////////
-
-// add event listeners to art_pieces
-for (var i = 0; i < art_pieces.length; i++) {
-    art_pieces[i].addEventListener('click', function(event) {click_artpiece(event);}, false);
-    if (getWindowWidth().x > 600) {
-        art_pieces[i].addEventListener('mouseover', function(event) {mouseover_artpiece(event);}, false);
-        art_pieces[i].addEventListener('mouseout', function(event) {mouseout_artpiece(event);}, false);
-    }
-}
-
-// add event listeners to toggle_switches
-for (var i = 0; i < toggle_switches.length; i++) {
-    toggle_switches[i].addEventListener('click', function(event) {toggleCategory(event);}, false);
-}
-
-// add event listeners to tag buttons
-for (var i = 0; i < tag_buttons.length; i++) {
-    tag_buttons[i].addEventListener('click', function(event) {showCategory(event);}, false);
-}
-
-// add event listeners to about buttons
-for (var i = 0; i < about_buttons.length; i++) {
-    about_buttons[i].addEventListener('click', function(event) {showAboutSection(event);}, false);
-}
 
 function click_artpiece (event) {
 
@@ -518,7 +567,6 @@ function generateLanguageDataVisual () {
     // remove coffeescript
     language_names.splice(language_names.indexOf('CoffeeScript'), 1);
 
-    var center = new Vector(gallery.parentNode.clientWidth / 2, gallery.parentNode.clientHeight / 2);
     var angle_deg = 0;
 
     for (var i = 0; i < language_names.length; i++) {
@@ -537,10 +585,25 @@ function generateLanguageDataVisual () {
         skill_lang.innerHTML = language.lang + '';
         skill_data.innerHTML = language.lang_percent + '%';
 
+        skill_div.style.backgroundColor = language_colors[language_names[i]];
+
+
         // ISSUE, wait for values to be returned by ajax!!!!!!!!!
         // animate
         if (getWindowWidth().x > 900)
-            animateLanguage(skill_div, skill_data, skill_lang, center, language_names, language, angle_deg);
+            animateLanguage(skill_div, skill_data, skill_lang, language_names, language, angle_deg);
+        else {
+            setVendorPrefixForTransform(skill_div, 'rotate(' + (angle_deg * 0.05) + 'deg)');
+
+            skill_div.style.left = '0em';
+            skill_div.style.top = '0em';
+            skill_lang.style.opacity = '1';
+            skill_data.style.opacity = '1';
+            skill_div.style.width = language.lang_percent + '%';
+        }
+
+        skill_data.dataset.bytes = language.lang_bytes + " Bytes";
+        skill_data.dataset.percent = language.lang_percent + " %";
 
         skill_div.appendChild(skill_lang);
         skill_data.addEventListener('click', function(event) {switchLanguageDataFormat(event);});
@@ -555,15 +618,23 @@ function languageObject(lang, lang_bytes, lang_percent) {
     this.lang_percent = parseFloat(lang_percent, 10).toFixed(2)
 }
 
-function animateLanguage(skill_div, skill_data, skill_lang, center, language_names, language, angle_deg) {
+function animateLanguage(skill_div, skill_data, skill_lang, language_names, language, angle_deg) {
 
     var cycle = 0;
 
-    setVendorPrefixForTransformOrigin(skill_div, '-150px 0');
+    setVendorPrefixForTransformOrigin(skill_div, '0 0');
 
     var skill_div_width = language.lang_percent;
 
-    skill_div.style.backgroundColor = language_colors[language.lang];
+
+    skill_div.style.borderRadius = '100%';
+    skill_div.style.top = '100%';
+    skill_div.style.left = '80%';
+    skill_div.style.position = 'absolute';
+    skill_lang.style.opacity = '0';
+    skill_data.style.opacity = '0';
+    skill_div.style.width = '1em';
+
 
     function step(timestamp) {
 
@@ -571,21 +642,22 @@ function animateLanguage(skill_div, skill_data, skill_lang, center, language_nam
 
         if (cycle <= 360) { // do for duration
 
+            skill_div.style.top = '3em';
             requestAnimationFrame(step);
         }
-        else if (cycle <= 720) { // do for duration
+        else if (cycle <= 720) { // do for duration;
+            skill_div.style.left = '3em';
             requestAnimationFrame(step);
         }
         else if (cycle <= 1080) { // do for duration
-            setVendorPrefixForTransform(languages_breakdown, 'rotate(' + (cycle - 720) + 'deg)');
-            setVendorPrefixForTransform(skill_div, 'rotate(' + (1.01 * (cycle - 720)) + 'deg)');
-            // setVendorPrefixForTransform(skill_div, 'rotate(' + (cycle - 720) + 'deg)');
+            setVendorPrefixForTransform(languages_breakdown, 'rotate(' + -(cycle - 720) + 'deg)');
+            setVendorPrefixForTransform(skill_div, 'rotate(' + (1.01 * (cycle - 720) + (angle_deg * 0.05)) + 'deg)');
 
             requestAnimationFrame(step);
         }
         else if (cycle <= 1440) {
+            skill_div.style.top = '1em';
             skill_div.style.position = 'relative';
-
             requestAnimationFrame(step);
         }
         else if (cycle <= 1600) {
@@ -666,11 +738,6 @@ window.mobilecheck = function() {
   var check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
-}
-
-function Vector(x, y) {
-    this.x = x;
-    this.y = y;
 }
 
 function ajaxCall(api, command, node) {
